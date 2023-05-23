a6801f79e982b1fcb9e46e1d6fe09e32e07c967a
luke.morgan <luke.morgan@madgex.com>
2023-05-23 17:05:09 +0100

    no message

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
-import AsyncHelloWorld from '@/components/AsyncHelloWorld.vue';
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue + Vitest + Vue Test Utils" />
-  <!-- <AsyncHelloWorld /> -->
 </template>
 
 <style scoped></style>
